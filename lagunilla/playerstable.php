dcab5b3959226731b2240b1297ca2fa4aea33e2b
zmonya <117254872+zmonya@users.noreply.github.com>
2024-04-28 13:16:03 +0800

    Add files via upload

---
+++ lagunilla/playerstable.php
@@ -0,0 +1,17 @@
+<!-- table -->
+<table class="table" id="userstable">
+  <thead>
+    <tr>
+      <th scope="col">Student ID</th>
+      <th scope="col">Picture</th>
+      <th scope="col">Name</th>
+      <th scope="col">Email</th>
+      <!-- <th scope="col">Student ID</th> -->
+      <th scope="col"></th>
+    </tr>
+  </thead>
+  <tbody>
+
+  </tbody>
+</table>
+<!-- table -->
